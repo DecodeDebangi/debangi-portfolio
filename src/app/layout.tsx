b33a0aeb4b78e2603c30b25a5c77d56c5d41413c
Debangi <debangiofficial@gmail.com>
2025-02-09 01:28:46 +0530

    Initial commit2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Debangi Portfolio",
   description: "Created with the help of Frontend Tribe",
 };
 
