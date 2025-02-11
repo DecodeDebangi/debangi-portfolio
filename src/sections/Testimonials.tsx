a616859a34e80aa8a14bfbeeb922af150ea2e9e8
Debangi <debangiofficial@gmail.com>
2025-02-11 15:28:34 +0530

    created section header component

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,6 +3,8 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import Image from "next/image";
+import { SectionHeader } from "@/components/SectionHeader";
 
 const testimonials = [
   {
@@ -38,5 +40,24 @@ const testimonials = [
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return (
+    <div>
+      <SectionHeader
+        eyebrow='Happy Clients'
+        title='What Clients Say about me'
+        description="Don't just take my word for it. See what my clients have to say about my
+        work."
+      />
+      <div>
+        {testimonials.map((testimonial, index) => (
+          <div key={index}>
+            <Image src={testimonial.avatar} alt={testimonial.name} />
+            <div>{testimonial.name}</div>
+            <div>{testimonial.position}</div>
+            <div>{testimonial.text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
