8eb140a68f1329d49710f6dd4dcd3484671b4843
Debangi <debangiofficial@gmail.com>
2025-02-11 14:40:00 +0530

    tape finalised

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -9,15 +9,20 @@ const words = [
   "NextJs",
   "ReactJs",
   "Token",
+  "Solidity",
+  "Smart Contract",
+  "Blockchain",
+  "Ethereum",
+  "Solana",
 ];
 export const TapeSection = () => {
   return (
-    <div className='py-16'>
+    <div className='py-16 lg:py-24 overflow0-x-clip'>
       <div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1'>
         <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
-          <div className='flex flex-none gap-4 py-3'>
+          <div className='flex flex-none gap-6 py-3'>
             {words.map((word, index) => (
-              <div key={index} className='inline-flex gap-4 items-center'>
+              <div key={index} className='inline-flex gap-6 items-center'>
                 <span className='text-gray-900 uppercase font-extrabold text-sm'>
                   {word}
                 </span>
